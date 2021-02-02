b8016c7ee457f328eed8471672584795830eac96
Dmitry Halavach <halawacz@gmail.com>
2021-02-02 23:40:40 +0300

    added recursive solution of the reverse integer task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const nString = n.toString();
+
+    function rev(s) {
+        if (s == "") {
+            return "";
+        } else {
+            return rev(s.substr(1)) + s.charAt(0);
+        }
+    }
+
+    return parseInt(rev(nString));
+};
